e10af2fc690404fd6dde690f26347516c1fd045c
shekelashvili-st <sergey200004@gmail.com>
2025-06-20 22:43:17 +0300

    Add logging to global handler

--- src/main/java/ru/practicum/shareit/handlers/GlobalExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/handlers/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.handlers;
 
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,30 +13,38 @@ import ru.practicum.shareit.exception.IdMismatchException;
 import ru.practicum.shareit.exception.IdNotFoundException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleBadRequest(EmailAlreadyExistsException e) {
-        return new ApiError(e.getMessage());
+        String message = e.getMessage();
+        log.warn("Email already exists: {}", message);
+        return new ApiError(message);
     }
 
     @ExceptionHandler({IdNotFoundException.class, IdMismatchException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleIdNotFound(RuntimeException e) {
-        return new ApiError(e.getMessage());
+        String message = e.getMessage();
+        log.warn("Incorrect id: {}", message);
+        return new ApiError(message);
     }
 
-    @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class})
+    @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class, ConstraintViolationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleValidation(Throwable e) {
-        return new ApiError(e.getMessage());
+        String message = e.getMessage();
+        log.warn("Validation error occurred: {}", message);
+        return new ApiError(message);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError handleUncaught(Throwable e) {
         String message = e.getMessage();
+        log.warn("Unexpected error occurred: {}", message);
         return new ApiError(message);
     }
 
